6b8a4ffe12a9bf99e70307cb07c29aab25fbc51f
Abdollah <abefarsane@gmail.com>
2023-02-02 03:01:53 +0100

    Fixed bugs, maybe?

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -88,7 +88,10 @@ export default function Signup() {
                 username: username,
                 pwd: pwd
             }
-            axios.post("https://morocco-my-trip-api.herokuapp.com/auth/signup", { headers :{
+            axios.post({method: 'post',
+            baseURL: 'https://morocco-my-trip-api.herokuapp.com/',
+            url: 'auth/signup'},
+            { headers :{
                 'Content-Type': 'application/json'
             }}, user ).then((response) => {
                 if(response.data.status) {
